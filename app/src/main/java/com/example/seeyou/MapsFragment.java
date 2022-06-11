3f9e39a05e3178136a07b70a41f8a535c0482a3a
EdgarLopez13 <78750255+EdgarLopez13@users.noreply.github.com>
2022-06-10 17:52:19 -0700

    Correccion de ubicacion

--- app/src/main/java/com/example/seeyou/MapsFragment.java
+++ app/src/main/java/com/example/seeyou/MapsFragment.java
@@ -2,8 +2,10 @@ package com.example.seeyou;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -13,6 +15,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -23,44 +26,60 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.navigation.NavigationView;
 
 public class MapsFragment extends Fragment {
     public static GoogleMap mMap;
     LocationManager locManager;
-    private ImageView ubicacion,location;
-    private Button cancelar,enviar;
-    public static double LatitudDialogo,LongitudDialogo;
+    private ImageView ubicacion, location;
+    private Button cancelar, enviar;
+    public static double LatitudDialogo, LongitudDialogo;
+    private LinearLayout contenedor;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
 
-        @SuppressLint("MissingPermission")
+
         @Override
         public void onMapReady(GoogleMap googleMap) {
             mMap = googleMap;
 
-            //Da acceso de permiso a la ubicacion
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
             locManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
 
+            int permisos = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION);
 
-            //Guarda los valores de longitud y latitud en variables
-            @SuppressLint("MissingPermission")
-            Location loc = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            double lat = loc.getLatitude();
-            double lon = loc.getLongitude();
 
-            LatLng UbicacionActualo = new LatLng(lat, lon);
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UbicacionActualo,14));
+            if (permisos == PackageManager.PERMISSION_GRANTED) {
 
+                Location loc = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (loc != null) {
+                //Guarda los valores de longitud y latitud en variables
+                double lon = loc.getLongitude();
+                double lat = loc.getLatitude();
+
+                //Crea el punto donde se marcara
+                LatLng UbicacionActualo = new LatLng(lat, lon);
+
+                //Mueve la camara al punto proporcionado, osea la ubicacion del usuario
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UbicacionActualo, 14));
+            }
+            } else{
+                Toast.makeText(getContext(), "NO SE A PODIDO OPTENER LA UBICACION", Toast.LENGTH_SHORT).show();
+            }
 
 
             //LatLng sydney = new LatLng(31.233544, -110.979941);
@@ -73,13 +92,31 @@ public class MapsFragment extends Fragment {
 
             //Habilita el ver la ubicacion actual
             mMap.setMyLocationEnabled(true);
-           // mMap.getUiSettings().setZoomControlsEnabled(true);
+            // mMap.getUiSettings().setZoomControlsEnabled(true);
 
             //Habilida la funcion del compas, cuando gires la pantalla podras volver a orientarla
             mMap.getUiSettings().setCompassEnabled(true);
 
             //Desabilita el boton que te envia a la ubicacion tuya (porque cree un boton mas estetico para eso)
             mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+            mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                @Override
+                public boolean onMarkerClick(@NonNull Marker marker) {
+
+                    BottomSheetDialog bottomSheetDialog = new BottomSheetDialog
+                            (getContext(), R.style.BottomSheetDialog);
+                    View bottomSheetView = LayoutInflater.from(getContext()).inflate(
+                            R.layout.fragment_tarjeta, (LinearLayout) contenedor
+                    );
+                    bottomSheetDialog.setContentView(bottomSheetView);
+
+
+                    bottomSheetDialog.show();
+
+                    return true;
+                }
+            });
 
 
         }
@@ -88,7 +125,7 @@ public class MapsFragment extends Fragment {
     };
 
 
-    public void Marcador(){
+    public void Marcador() {
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(@NonNull LatLng latLng) {
@@ -111,16 +148,16 @@ public class MapsFragment extends Fragment {
                 markerOptions.position(latLng);
 
                 //le da nombre al punto
-                markerOptions.title(latLng.latitude+" : " +latLng.longitude);
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
 
 
                 //Mueve la camara al punto proporcionado, osea la ubicacion del usuario
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,14));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14));
 
 
                 //llama al fragmento dialogo
                 Dialogo_MensajeFragment dialogofragment = new Dialogo_MensajeFragment();
-                dialogofragment.show(getFragmentManager(),"MyFragment");
+                dialogofragment.show(getFragmentManager(), "MyFragment");
 
 
             }
@@ -128,66 +165,64 @@ public class MapsFragment extends Fragment {
     }
 
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_maps, container, false);
 
-        @Nullable
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater,
-                                 @Nullable ViewGroup container,
-                                 @Nullable Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment_maps, container, false);
-
-            ubicacion = view.findViewById(R.id.IVubicacion);
-            cancelar = view.findViewById(R.id.BTNcancelar);
-            enviar = view.findViewById(R.id.BTNenviarmensaje);
-            location = view.findViewById(R.id.IVlocation);
-
-
+        ubicacion = view.findViewById(R.id.IVubicacion);
+        cancelar = view.findViewById(R.id.BTNcancelar);
+        enviar = view.findViewById(R.id.BTNenviarmensaje);
+        location = view.findViewById(R.id.IVlocation);
+        contenedor = view.findViewById(R.id.ContenedorTarjeta);
 
 
-            location.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-                    //Da acceso de permiso a la ubicacion
-                    locManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-
-
-                    //Optiene la ultima localizacion conocida del usuario
-                    @SuppressLint("MissingPermission")
-                    Location loc = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-                    //Guarda los valores de longitud y latitud en variables
-                    double lat = loc.getLatitude();
-                    double lon = loc.getLongitude();
-
-                    //Crea el punto donde se marcara
-                    LatLng UbicacionActualo = new LatLng(lat, lon);
-
-                    //Mueve la camara al punto proporcionado, osea la ubicacion del usuario
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UbicacionActualo,14));
-                }
-            });
+                //Da acceso de permiso a la ubicacion
+                locManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
 
 
-            cancelar.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+                //Optiene la ultima localizacion conocida del usuario
+                @SuppressLint("MissingPermission")
+                Location loc = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-                    //desactiva la funcion de marcar puntos al hacer click
-                    mMap.setOnMapClickListener(null);
+                //Guarda los valores de longitud y latitud en variables
+                double lat = loc.getLatitude();
+                double lon = loc.getLongitude();
 
-                    //vuelve invisible el boton de cancelar
-                    cancelar.setVisibility(View.INVISIBLE);
+                //Crea el punto donde se marcara
+                LatLng UbicacionActualo = new LatLng(lat, lon);
 
-                }
-            });
+                //Mueve la camara al punto proporcionado, osea la ubicacion del usuario
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UbicacionActualo, 14));
+            }
+        });
 
-            ubicacion.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
 
-                    //vuelve visible el boton de cancelar
-                    cancelar.setVisibility(View.VISIBLE);
+        cancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //desactiva la funcion de marcar puntos al hacer click
+                mMap.setOnMapClickListener(null);
+
+                //vuelve invisible el boton de cancelar
+                cancelar.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+        ubicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //vuelve visible el boton de cancelar
+                cancelar.setVisibility(View.VISIBLE);
 
                     /*hacia una prueba donde bajaba el brillo a la pantalla cuando hacia clic a esta imagen,
                      funciona pero no tiene utilidad de momento
@@ -198,22 +233,24 @@ public class MapsFragment extends Fragment {
                     getActivity().getWindow().setAttributes(brillo);
                     */
 
-                    Marcador();
-                }
-            });
-
-            return view;
-        }
-
-        @Override
-        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-            super.onViewCreated(view, savedInstanceState);
-            SupportMapFragment mapFragment =
-                    (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-            if (mapFragment != null) {
-                mapFragment.getMapAsync(callback);
+                Marcador();
             }
+        });
 
+        return view;
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
         }
 
     }
+}
+
+
